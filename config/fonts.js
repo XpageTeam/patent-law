fd78c266e9bf8eeadb53655f81e0860df35879a5
SergeyG <sergey1008@gmail.com>
2019-04-23 17:52:45 +0500

    сверстал  часть главной

--- config/fonts.js
+++ config/fonts.js
@@ -1,5 +1,5 @@
 module.exports = {
-	formats: 'local woff2',
+	formats: 'local woff woff2',
 	display: "swap",
 	custom: {
 		"Montserrat": {
@@ -7,27 +7,36 @@ module.exports = {
 				normal: {
 					300: {
 						url: {
-							woff: "../fonts/montserrat-light.woff2",
+							woff: "../fonts/Montserrat/montserrat-light.woff",
+							woff: "../fonts/Montserrat/montserrat-light.woff2",
 						}
 					},
 					400: {
 						url: {
-							woff: "../fonts/montserrat-regular.woff2",
+							woff: "../fonts/Montserrat/montserrat-regular.woff",
+							woff: "../fonts/Montserrat/montserrat-regular.woff2",
 						}
 					},
 					500: {
 						url: {
-							woff: "../fonts/montserrat-medium.woff2",
+							woff: "../fonts/Montserrat/montserrat-medium.woff",
+							woff: "../fonts/Montserrat/montserrat-medium.woff2",
 						}
 					},
-					900: {
+					600: {
 						url: {
-							woff: "../fonts/montserrat-bold.woff2",
+							woff: "../fonts/Montserrat/montserrat-semibold.woff",
+							woff: "../fonts/Montserrat/montserrat-semibold.woff2",
+						}
+					},
+					700: {
+						url: {
+							woff: "../fonts/Montserrat/montserrat-bold.woff",
+							woff: "../fonts/Montserrat/montserrat-bold.woff2",
 						}
 					},
 				}
 			}
 		},
-
 	}
 }
